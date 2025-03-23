3e6ad12c72af12639c98cdb0638f753d94079af1
Mbeleck Berle <mbeleckmbeleck@gmail.com>
2025-03-23 16:45:49 -0400

    --

---
+++ routing-demo/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound(){
+    return (
+        <div>
+            <h1>
+                Page not found
+            </h1>
+            <p> couldn not find requested resource</p>
+        </div>
+    )
+}
+
